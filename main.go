da5ec128e3e33dd71ac6537f2acab75e1c19c4ee
welingtoncassis <welingtoncassis@gmail.com>
2021-12-18 12:10:50 -0300

    add struct product and passing to the template

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Amount      int
+}
+
 var temps = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,6 +20,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	products := []Product{
+		{"T-Shirt", "Black", 29.9, 10},
+		{"Short", "Blue", 19.9, 5},
+		{"Shoes", "Red", 89.9, 3},
+	}
 	// Parms: w(quem responde), index(html), valor passado para o html
-	temps.ExecuteTemplate(w, "Index", nil)
+	temps.ExecuteTemplate(w, "Index", products)
 }
