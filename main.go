d61becb54089bfb03d4d9799e767f560928e85c8
welingtoncassis <welingtoncassis@gmail.com>
2021-12-18 12:57:56 -0300

    add docker-compose and connection with db

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 	"text/template"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func connectDB() *sql.DB {
+	user := goDotEnvVariable("DB_USER")
+	password := goDotEnvVariable("DB_PASSWORD")
+	dbName := goDotEnvVariable("DB_NAME")
+	host := goDotEnvVariable("DB_HOST")
+	sslmode := goDotEnvVariable("DB_SSLMODE")
+
+	connectionString := "user=" + user + " dbname=" + dbName + " password=" + password + " host=" + host + "sslmode" + sslmode
+
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Product struct {
 	Name        string
@@ -15,6 +50,8 @@ type Product struct {
 var temps = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := connectDB()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
